f286b85654ef87e579e87c2dc5256a503cb59561
Kashmala-Asad <kashmalaasad84@gmail.com>
2025-12-30 02:19:31 +0500

    Update doctors.service.ts

--- src/modules/doctors/doctors.service.ts
+++ src/modules/doctors/doctors.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { Doctor, DoctorDocument } from '../../common/schemas/doctor.schema';
+
 
 @Injectable()
 export class DoctorsService {
